58d97941903e41a2a8e5968bb4dbac59e23fb4f5
kehul07 <kehulsali2004@gmail.com>
2024-04-02 19:12:21 +0530

    end

--- server/app.js
+++ server/app.js
@@ -15,7 +15,6 @@ const Conversation = require("./models/Conversations");
 const Messages = require("./models/Messages");
 
 const port = process.env.PORT || 8000;
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
